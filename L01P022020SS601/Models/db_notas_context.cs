ee5a98b870c3f01c35be77caeba62a3bad71d2d0
jserr <jserr@JC>
2024-04-06 12:54:28 -0600

    Agregar archivos de proyecto.

---
+++ L01P022020SS601/Models/db_notas_context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace L01P022020SS601.Models
+{
+    public class db_notas_context : DbContext
+    {
+        public db_notas_context(DbContextOptions options) : base(options)
+        {
+
+    }
+        public DbSet<facultades> facultades { get; set; }
+
+
+    }
+}
---
+++ L01P022020SS601/Models/facultades.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace L01P022020SS601.Models
+{
+    public class facultades
+    {
+        [Key]
+        [Display(Name = "ID")]
+        public int id { get; set; }
+
+        [Display(Name = "Nombre de la Facultad")]
+        [Required(ErrorMessage = "El nombre de la Facultad No es opcional")]
+        public string? facultad { get; set; }
+
+    }
+}
